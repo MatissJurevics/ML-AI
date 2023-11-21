1f1da8d14c2ca12c81313e49b167e2fb6aa46b58
matiss jurevics <matissjurevics@gmail.com>
2023-11-21 12:21:26 +0000

    improved loading bars

--- mnist.py
+++ mnist.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from models.perceptron import Perceptron
+from tqdm import tqdm
 
 models = {}
 
@@ -11,20 +12,18 @@ X,y = mnist.data, mnist.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 
-for i in range(10):
+for i in tqdm(range(10)):
     models[i] = Perceptron(learning_rate=0.005)
     models[i].fit(X_train, np.where(y_train==i, 1, 0))
-    print(f'{i}/10: Model Trained', end="\r")
 
 predictions = []
 
-for i in range(10):
+for i in tqdm(range(10)):
     predictions.append([models[i].predict_ovr(X_test)])
-    print(f'{i}/10', end="\r")
 
 y_pred = np.zeros(len(X_test))
 
-for i in range(len(X_test)):
+for i in tqdm(range(len(X_test))):
     y_pred[i] = np.argmax([predictions[j][0][i] for j in range(10)])
     
 
